0e066aaf58d67ecd7e6e190a5b23b7e19339778f
PuntikaL <89716947+PuntikaL@users.noreply.github.com>
2024-02-14 15:25:03 +0700

    Add files via upload

---
+++ possiblematch.js
@@ -0,0 +1,3 @@
+document.getElementById('okayButton').addEventListener('click', function() {
+    window.location.href = 'gotoform.html'; // Redirect to follow_up_questions.html when "Okay!" is clicked
+});
