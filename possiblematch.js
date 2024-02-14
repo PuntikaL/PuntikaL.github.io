3901a5d4ffbd47f30a82978ce201abad1109a01e
PuntikaL <89716947+PuntikaL@users.noreply.github.com>
2024-02-14 15:54:29 +0700

    Update possiblematch.js

--- possiblematch.js
+++ possiblematch.js
@@ -1,3 +1,7 @@
-document.getElementById('okayButton').addEventListener('click', function() {
-    window.location.href = 'gotoform.html'; // Redirect to follow_up_questions.html when "Okay!" is clicked
-});
+document.getElementById('yesButton').addEventListener('click', function() {
+    window.location.href = 'gotoform.html';
+})
+
+document.getElementById('noButton').addEventListener('click', function() {
+    window.location.href = 'decline.html';
+})
